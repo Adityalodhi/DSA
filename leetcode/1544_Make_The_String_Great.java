005e8b1aedd8523d84174673e64552c952770d51
Adityalodhi <adityalodhi2003@gmail.com>
2024-04-05 16:24:13 +0530

    question number 1544

---
+++ leetcode/1544_Make_The_String_Great.java
@@ -0,0 +1,67 @@
+// Given a string s of lower and upper case English letters.
+
+// A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+
+// 0 <= i <= s.length - 2
+// s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+// To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+
+// Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+
+// Notice that an empty string is also good.
+
+ 
+
+// Example 1:
+
+// Input: s = "leEeetcode"
+// Output: "leetcode"
+// Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+// Example 2:
+
+// Input: s = "abBAcC"
+// Output: ""
+// Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+// "abBAcC" --> "aAcC" --> "cC" --> ""
+// "abBAcC" --> "abBA" --> "aA" --> ""
+// Example 3:
+
+// Input: s = "s"
+// Output: "s"
+
+
+class Solution {
+    public String makeGood(String s) {
+
+        //stack approach
+        // StringBuilder sb=new StringBuilder();
+        // Stack<Character> st = new Stack<>();
+
+        // for(char ch : s.toCharArray()){
+        //     if(!st.isEmpty() && Math.abs(ch-st.peek()) == 32){
+        //         // System.out.println("Aditya");
+        //         st.pop();
+        //     }
+        //     else{
+        //         st.push(ch);
+        //     }
+        // }
+
+        // while(!st.isEmpty()){
+        //     sb.insert(0,st.pop());
+        // }
+
+        // return sb.toString();
+
+
+
+        //recursive approach...
+        for(int i=0;i<s.length()-1;i++){
+            if(Math.abs(s.charAt(i) - s.charAt(i+1)) ==32){
+               return makeGood(s.substring(0,i) + s.substring(i+2));
+            }
+        }
+
+        return s;
+    }
+}
